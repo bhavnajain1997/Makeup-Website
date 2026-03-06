b8ecd7f8d419f08543dc56dcbadccde61c2adf2c
Bhavna <jbhavna732@gmail.com>
2026-03-06 16:38:19 +0530

    Update package.json for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";  // 1. Import karo
 import AppLayout from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppLayout />
+    <BrowserRouter basename="/Makeup-Website"> {/* 2. Wrap karo */}
+      <AppLayout />
+    </BrowserRouter>
   </React.StrictMode>
 );
